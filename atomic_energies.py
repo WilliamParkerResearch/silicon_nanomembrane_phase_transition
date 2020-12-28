5bdd21e51614fa8aa21a8436a0d7df364fa0ec92
williamparker <baiweilian@gmail.com>
2020-12-28 13:40:49 -0600

    Created file to hold (pseudo)atomic calculation data

---
+++ atomic_energies.py
@@ -0,0 +1,13 @@
+atomic_calculations = {
+    1: {"pseudopotential": "Si.pz-n-rrkjus_psl.0.1.UPF",        "total energy": -10.98894304},
+    2: {"pseudopotential": "Si.pbe-nl-kjpaw_psl.1.0.0.UPF",     "total energy": -46.39167521},
+    3: {"pseudopotential": "Si.pbesol-nl-kjpaw_psl.1.0.0.UPF",  "total energy": -45.27537292},
+}
+
+for id, info in atomic_calculations.items():
+    print("\nCalculation ID:", id)
+    for key in info:
+        if key == "total energy":
+            print(key + ':', info[key], 'Ry')
+        else:
+            print(key + ':', info[key])
